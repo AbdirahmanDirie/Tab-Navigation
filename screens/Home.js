ea8e17b01ed24b477581ddd086f9fddc253ae7ed
abdirahman dirie <cabdirahmandirie.com>
2023-01-29 12:25:56 +0300

    Tab navifation

---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+function Home (){
+    return (
+      <View>
+        <Text>Home Screen</Text>
+      </View>
+    )
+  }
+
+  export default Home
+  
---
+++ screens/Movie.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+function Movies (){
+    return (
+      <View>
+        <Text>Movies Screen</Text>
+      </View>
+    )
+  }
+
+  export default Movies
